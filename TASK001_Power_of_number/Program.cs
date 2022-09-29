21993ffc6ef3375ca5d757749c51927083147bfb
Victor Rusaev <rusaev74@mail.ru>
2022-09-29 19:06:33 +0300

    TASK001 Power of number

---
+++ TASK001_Power_of_number/Program.cs
@@ -0,0 +1,22 @@
+﻿int Power(int num1, int num2)
+{
+    int index = 2;
+    int result = num1;
+    while(index <= num2)
+    {
+        result = result * num1;
+        index++;
+    }
+    return result;
+}
+
+Console.Clear();
+
+Console.Write("Введите число A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число B: ");
+int B = int.Parse(Console.ReadLine()!);
+
+int C = Power(A, B);
+Console.WriteLine(C);
